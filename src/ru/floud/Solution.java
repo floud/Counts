338990a6abb9554908b7f16593cba68d8be36ef7
floud <ahalturin@bk.ru>
2014-09-11 21:12:10 +0400

    final

--- src/ru/floud/Solution.java
+++ src/ru/floud/Solution.java
@@ -1,3 +1,5 @@
+package ru.floud;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,19 +11,17 @@ public class Solution {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Программа подсчета количества разбиений числа на заданное число слагаемых. Введите число: ");
         int n = Integer.parseInt(reader.readLine());
+        System.out.println("Введите число слагаемых: ");
         int k = Integer.parseInt(reader.readLine());
         System.out.println(count(n, k));
-
-
     }
 
     public static int count(int n, int k) {
         if (n == k) return 1;
         if (k == 1) return 1;
         if (k > n) return 0;
-        int sum = count(n - 1, k -1) + count(n - k, k);
-        return sum;
-
+        return count(n - 1, k - 1) + count(n - k, k);
     }
 }
